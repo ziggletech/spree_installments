d8c15e73b6327e8c0f459363ac6739e5af95b413
paras <paras@ziggletech.com>
2018-07-16 12:14:09 +0530

    Added button to capture all installments at once

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -7,4 +7,34 @@ Spree::Admin::OrdersController.class_eval do
            installment.capture!
         redirect_to request.referer
       end
+
+      def captureAllInstallments
+        #session[:return_to] ||= request.referer
+           @id = params[:id]
+           amountToBeCaptured = 0.00
+           installmentPlan =  Spree::InstallmentPlan.find_by_id(@id)
+           shipment = installmentPlan.shipment
+           order = shipment.order
+           installmentPlan.installments.where("paid_at IS NULL").each do |installment|
+            amountToBeCaptured = amountToBeCaptured + installment.amount
+           end
+           
+           payment = order.create_shipment_payment(amountToBeCaptured, order.authorized_payment, shipment.id)
+
+           payment.purchase!
+
+          if payment.completed?
+            installmentPlan.installments.where("paid_at IS NULL").each do |installment|
+                installment.update_column(:paid_at, Time.zone.now)
+                installment.update_column(:state, "completed")
+               end
+          else
+            installmentPlan.installments.where("paid_at IS NULL").each do |installment|
+                installment.update_column(:state, "failed")
+               end
+          end
+
+        redirect_to request.referer
+      end
+      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   get  '/captureinstalment/:id' => 'admin/orders#captureInstalment', as: 'admin_orders_captureInstalment' 
+  get  '/captureallinstallments/:id' => 'admin/orders#captureAllInstallments', as: 'admin_orders_captureAllInstallments' 
 end
